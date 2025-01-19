3b153b2b1163384149e65097872831db57e63640
marklanselot <markrybitskij@gmail.com>
2025-01-19 18:40:26 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="cybershoke",
-    version="0.1.4",
+    version="0.1.5",
     description="",
     author="marklanselot",
     author_email="markrybitskij@gmail.com",
